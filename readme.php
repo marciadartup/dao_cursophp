769e95816598409f7686ff554abcb2c9596f1072
Marcia Alves <marcia.darts@gmail.com>
2022-02-22 13:35:36 -0300

    alteracoes novas nas classes

--- readme.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//testando atualização do commit
-echo "<h1>Alo Mundo!  Módulo Dao </h1>"
-
-?>
--- xxxindex.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//$men = "Alo Mundo! . Meu primeiro Projeto em PHP"
-echo "<h1>Alo Mundo!  Módulo Dao </h1>"
-
-?>
